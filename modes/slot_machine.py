cc9cbf6d6246f2f253632a2ce19df07f3fe1577c
MrDotsss <nunezclifford28@gmail.com>
2026-06-28 15:55:13 +0800

    SLOT MACHINE: End messages

--- modes/slot_machine.py
+++ modes/slot_machine.py
@@ -1,8 +1,9 @@
+import math
 import random
 import time
 
 from core.mode_manager import ModeManager, BaseMode
-from core.tools import clear_console, yes_no_query_invoker, get_non_empty_int_range_input
+from core.tools import clear_console, yes_no_query_invoker, get_non_empty_int_range_input, get_non_empty_float_input
 
 
 class SlotMachineGame(BaseMode):
@@ -10,6 +11,7 @@ class SlotMachineGame(BaseMode):
         super().__init__()
         self.max_slots: int = 3
         self.spin_cost: float = 1
+        self.starting_balance: float = 0
         self.current_score: float = 0
         self.current_multiplier: int = 1
         self.slot_icons: list[str] = ["🍕", "🍗", "🍉", "🌭", "🍔", "🎉", "🧨"]
@@ -23,7 +25,7 @@ class SlotMachineGame(BaseMode):
     def start(self, mode_manager: ModeManager) -> None:
         super().start(mode_manager)
         clear_console()
-        self.current_score = 0
+        self.starting_balance = 0
         self.current_multiplier = 1
         self.current_bet_slot = None
         self.current_spin_slot.clear()
@@ -31,6 +33,7 @@ class SlotMachineGame(BaseMode):
 
         self.instructions()
 
+        self.current_score = self.starting_balance
         for i in range(self.max_slots):
             self.current_spin_slot.append(random.choice(self.slot_icons))
 
@@ -51,7 +54,7 @@ class SlotMachineGame(BaseMode):
     def __display_status(self) -> None:
         print("SPIN TO WIN!")
         print(f"Slots: {self.max_slots} | Spin Cost: {self.spin_cost:,.2f}\n")
-        print(f"Score: {self.current_score:,.2f}")
+        print(f"Balance: {self.current_score:,.2f}")
         print(f"Multiplier: {self.current_multiplier:.2f}")
         if self.current_bet_slot is not None:
             print(f"Bet slot: {self.current_bet_slot}")
@@ -150,6 +153,7 @@ class SlotMachineGame(BaseMode):
             print(slot_icon, end=" | ")
 
         print()
+        print("="*(5*self.max_slots))
         if len(matches) > 1 or bet_icon == self.slot_icons[-1]:
             icon_value: float = self.slot_icons.index(bet_icon) + 1 * (3 / self.max_slots)
             score_to_add: float = icon_value * len(matches)
@@ -171,12 +175,32 @@ class SlotMachineGame(BaseMode):
         print(f"Spinning {self.slot_icons[-1]} to bet slot automatically gain {len(self.slot_icons)} even without match!\n")
         time.sleep(0.5)
         print("More slots: High chance, low points | Less Slots: Low change, high points")
-        self.max_slots = get_non_empty_int_range_input("How many slots do you want to bet on? (3-10): ", 3, 10)
+        self.max_slots = get_non_empty_int_range_input("How many slots do you want to bet on? (3-5): ", 3, 5)
+        self.starting_balance = get_non_empty_float_input("Input starting balance: ")
         input("Press ENTER to Play the Game!\n")
         clear_console()
         time.sleep(1)
 
     def on_exit(self) -> None:
-        print("Thank you for checking this out!")
+        clear_console()
+        if self.current_score <= 0:
+            print("STOP IT, GET SOME HELP!")
+            print(f"YOU ARE IN DEPT OF {self.current_score:,.2f}") if self.current_score < 0 else print(f"YOU HAVE NOTHING LEFT! ZERO BALANCE")
+        elif self.starting_balance > self.current_score:
+            print(f"You start at {self.starting_balance:,.2f}, yet you only left with {self.current_score:,.2f}!")
+            print(f"You lost {self.starting_balance - self.current_score:,.2f}!")
+            print("Don't Gamble Again!")
+        else:
+            print(f"You start at {self.starting_balance:,.2f}, and left with {self.current_score:,.2f}!")
+            print(f"You won! {self.current_score - self.starting_balance:,.2f}!")
+
+            if int(self.current_score / self.starting_balance) > 1:
+                print(f"You multiplied your balance by {(self.current_score / self.starting_balance):,.2f} times!")
+                print("Lucky hooman")
+            else:
+                print("Pfft not that much :P")
+
+        time.sleep(2)
+        print("\nThank you for checking this out!")
         input("Press ENTER to return to main menu.")
         self.mode_manager.exit_mode()
